5b1b83c89e1bf06280e14de788ce83155080ae70
Ben <twelve1six@naver.com>
2014-07-15 14:25:23 +0900

    View 1     -> change type: text -> file & add enctype(important!!) SaveToDB -> Save img to DB as 0x binary form.

--- SaveToDB_Img.php
+++ SaveToDB_Img.php
@@ -1,13 +1,71 @@
 <html>
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<title>Save Image to DB</title>
+		<title>Display Image From DB</title>
 	</head>
-	
+
 	<body>
-		<form action="./DisplayFromDB_Img.php?mode=store" method="POST">
-			<p> image :	<input type="text" name="image"> </p>
-			<p> <input type="submit" /> </p>			
-		</form>		
+		<?php
+		
+		header("Content-Type: text/html; charset=euc-kr");
+		//기본 포트port 1433
+		$serverName = "192.168.0.20,1433";
+		
+		//USER & DB info
+		$DB_USERID = "sa";
+		$DB_USERPW = "h890910)";
+		$DB_NAME = "SAFE";
+
+		//$dbh = new PDO('sqlsrv:192.168.0.20,1433', $DB_USERID, $DB_USERPW, array('Database' => "SAFE"));
+		try {
+			// MySQL PDO 객체 생성
+			// mysql을 다른 DB로 변경하면 다른 DB도 사용 가능
+			$pdo = new PDO("sqlsrv:Server=$serverName;Database=$DB_NAME", $DB_USERID, $DB_USERPW);
+			// 에러 출력
+			$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (Exception $e) {
+			echo $e -> getMessage();
+		}
+
+		switch($_GET['mode']) {
+			case 'store' :
+				//query ready
+				$stmt = $pdo -> prepare("INSERT INTO Gallary (image, des) VALUES (:image, :des)");
+				
+				//check file exits
+				//echo $_POST['image']. "</br>";
+				//echo file_exists($_POST['image']);
+								
+				//set params
+				//$datastring = file_get_contents($_POST['image']);
+				// $image = file_get_contents($_POST['image']);
+				$des = 'default';
+				
+				//check params
+				// echo $image['hex'];;
+				// echo $image2;				
+				// echo $des;
+				
+				//check type
+				echo "</br>".$_FILES['image']['type'];
+				echo "</br>".file_get_contents($_FILES);				
+								
+				//bind params
+				$stmt -> bindParam(':image', file_get_contents($_FILES['image']['tmp_name']),PDO::PARAM_LOB, 0, PDO::SQLSRV_ENCODING_BINARY);
+				$stmt -> bindParam(':des', $des);
+							
+				//execute
+				$stmt -> execute();
+				
+				
+				//Display it from DB
+				
+				$stmt2 -> $pdo -> prepare("INSERT INTO Gallary (image, des) VALUES (:image, :des)");
+				
+								
+				break;
+		}
+		
+		?>
 	</body>
 </html>
---
+++ View1.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Save Image to DB</title>
+	</head>
+	
+	<body>
+		<form enctype="multipart/form-data" action="./SaveToDB_Img.php?mode=store" method="POST">
+			<p> image :	<input type="file" name="image"> </p>
+			<p> <input type="submit" /> </p>			
+		</form>		
+	</body>
+</html>
